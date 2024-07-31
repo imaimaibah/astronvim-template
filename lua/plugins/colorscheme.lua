c95772a46606e05d8478e47ff5f9c6db46438783
Shin Imai <binaryninja0101@yahoo.com>
2024-07-31 11:27:24 +0100

    Colorscheme configured & noice plugin installed

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      -- configuration options...
+    },
+  },
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "AstroNvim/astroui",
+    ---@type AstroUIOpts
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
