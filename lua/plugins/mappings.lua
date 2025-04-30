e21cbed0079c46b8669c399ef982590afb312b02
Shin Imai <binaryninja0101@yahoo.com>
2025-04-30 18:31:12 +0100

    Updated keymapping to get terminal up

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -7,10 +7,12 @@ return {
         -- first key is the mode
         n = {
           ["<C-q>"] = { "<C-v>", desc = "Visual Block" },
-          ["<C-\\>"] = { "<Cmd>ToggleTerm direction=float<CR>", desc = "Terminal float" },
+          ["<C-/>"] = { "<Cmd>ToggleTerm direction=float<CR>", desc = "Terminal float" },
+          --["<C-/>"] = { "<Cmd>ToggleTerm direction=horizontal<CR>", desc = "Terminal Bottom" },
         },
         t = {
           ["<C-\\>"] = { "<C-\\><C-n>", desc = "Normal mode" },
+          ["<C-/>"] = { "<Cmd>ToggleTerm<CR>", desc = "Terminal Bottom" },
         },
       },
     },
